5d261543563c324183120a02c69685fc0d30d538
Ivan Samardzic <isamar01@fesb.hr>
2021-03-29 18:05:44 +0200

    Add a window that wille pop up when trying to add new tech by clicking on AddBtn

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import M from 'materialize-css/dist/js/materialize.min.js'
+
+const AddTechModal = () => {
+    const [firstName, setFirstName] = useState()
+    const [lastName, setLastName] = useState()
+    const [tech, setTech] = useState('')
+
+    const onSubmit = () => {
+        if(firstName === '' || lastName === '') {
+            M.toast({ html: 'Please enter the name'});
+        } else {
+            console.log(lastName, lastName)
+            setFirstName('')
+            setLastName('')
+        }
+    }
+
+    return (
+        <div id='add-tech-modal' className='modal'>
+            <div className='modal-content'>
+                <h4>New Technician</h4>
+                <div className='row'>
+                    <div className='input-field'>
+                        <input type='text' name='firstName' value={firstName} onChange={e => setFirstName(e.target.value)} />
+                        <label htmlFor='firstName' className='active'>First Name</label>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='input-field'>
+                        <input type='text' name='lastName' value={lastName} onChange={e => setLastName(e.target.value)} />
+                        <label htmlFor='lastName' className='active'>Last Name</label>
+                    </div>
+                </div>
+            </div>
+            <div className='modal-footer'>
+                <a href='#!' onClick={onSubmit} className='modal-close waves-effect waves-blue blue btn'>Add Technician</a>
+            </div>
+        </div>
+    )
+}
+
+export default AddTechModal
