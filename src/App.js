5fe352daeebc8d7b58d6ed235ac95d36c9e16cf6
Ivan Samardzic <isamar01@fesb.hr>
2021-03-29 16:48:21 +0200

    Add components to main app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
-import React, { useEffect } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js'
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
 
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
-  
+   
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
