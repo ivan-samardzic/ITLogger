5f3802bc7aeedb7857ca543d107edc71b88da90b
Ivan Samardzic <isamar01@fesb.hr>
2021-03-31 18:19:48 +0200

    Add getTechs method, add logic inside actions and dispatch it to reducer

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -5,3 +5,32 @@ import {
     SET_LOADING,
     TECHS_ERROR
 } from './types'
+
+// Get techs
+export const getTechs = () => async dispatch => {
+    try {
+        setLoading();
+        const res = await fetch('/techs');
+        const data = await res.json();
+
+        dispatch({
+            type:GET_TECHS,
+            payload: data
+        });
+        } catch (err) {
+        dispatch({
+            type: TECHS_ERROR,
+            payload: err.response.data
+        })
+    }
+}
+
+
+ // Set Loading
+ export const setLoading = () => {
+    return { 
+        type: SET_LOADING
+    }
+}
+
+
--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,3 +1,11 @@
+import {
+    GET_TECHS,
+    ADD_TECH,
+    DELETE_TECH,
+    SET_LOADING,
+    TECHS_ERROR
+} from '../actions/types'
+
 const initialState = {
     techs: null,
     loading: false,
@@ -6,6 +14,17 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case GET_TECHS:
+            return {
+                ...state,
+                techs: action.payload,
+                loading: false
+            }
         default:
             return state;
     }
